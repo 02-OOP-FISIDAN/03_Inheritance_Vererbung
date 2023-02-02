87f27f9eb9eae3e7c23c9746cce572a26ba18e5a
FISIDAN <schwark@posteo.de>
2023-02-02 12:11:13 +0100

    functioning use of superclass

--- com/cc/java/Supercat.java
+++ com/cc/java/Supercat.java
@@ -1,5 +1,26 @@
 package com.cc.java;
 
 public class Supercat {
-    
+
+    private String name;
+    private String furColor;
+    protected int age; //protected ist zwichen private und public und ermöglicht die Sichtbarkeit in anderen *Sub-Klassen*
+   
+   
+    public Supercat(String name, String furColor, int age) { // Konstruktor
+        this.name = name;
+        this.furColor = furColor;
+        this.age = age;
+    }
+    public String getStringAttributes(String op) {  //Methode
+        switch (op) {
+          case "#name":
+            return name;
+          case "#color":
+            return furColor;
+          default:
+            return "ERROR!";
+        }
+      }
 }
+
